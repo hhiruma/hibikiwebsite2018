7d05cc6f8166332eb1f4050931713bf2e6ea2776
hyohyogo@yahoo.co.jp <hyohyogo@yahoo.co.jp>
2019-04-01 03:30:21 +0900

    FIX: minor fix

--- src/components/MenuBarSp.vue
+++ src/components/MenuBarSp.vue
@@ -4,6 +4,7 @@
     contain
     height="45px"
     src="https://firebasestorage.googleapis.com/v0/b/hibikiwebsite2018.appspot.com/o/images%2Fother%2F%E9%AD%81%E9%9F%BF%E3%83%AD%E3%82%B3%E3%82%99%E9%BB%92.png?alt=media&token=f6dd5c12-22d2-4c54-9fea-81e0913b29df"
+    @click="$store.commit('goToTop')"
   />
 
   <v-flex style="position: absolute; right: 0">
